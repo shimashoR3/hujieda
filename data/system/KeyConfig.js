de52752110d6dd57edf51de19b89b7b4d9fe7feb
ytouma1 <97589420+ytouma1@users.noreply.github.com>
2022-01-18 20:12:04 +0900

    Add files via upload

---
+++ data/system/KeyConfig.js
@@ -0,0 +1,31 @@
+var __tyrano_key_config = {
+	"key": {
+		"13": "next",
+		"17": "",
+		"32": "",
+		"91": ""
+	},
+	"mouse": {
+		"right": "hidemessage",
+		"center": "menu",
+		"wheel_up": "backlog",
+		"wheel_down": "next"
+	},
+	"gesture": {
+		"swipe_up_1": {
+			"action": "backlog"
+		},
+		"swipe_left_1": {
+			"action": "auto"
+		},
+		"swipe_right_1": {
+			"action": ""
+		},
+		"swipe_down_1": {
+			"action": "load"
+		},
+		"hold": {
+			"action": "skip"
+		}
+	}
+}
